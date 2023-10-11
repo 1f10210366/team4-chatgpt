8ac89abf0f286112fdec03556d5d7af26f7ca076
1f10210366 <85672478+1f10210366@users.noreply.github.com>
2023-10-11 15:04:25 +0900

    設定

---
+++ team4_chatgpt/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class ChatForm(forms.Form):
+
+  sentence = forms.CharField(label='チャット', widget=forms.Textarea(), required=True)
+
--- team4_chatgpt/views.py
+++ team4_chatgpt/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+from .forms import ChatForm
 
 def index(request):
   
-  return HttpResponse("Chatgpt")
+
+  form = ChatForm()
+  template = loader.get_template('team4_chatgpt/templates/team4_chargpt/index.html')
+  context = {
+    'form':form
+  }
+  return HttpResponse(template.render(context,request))
 
 
