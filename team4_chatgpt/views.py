668c094eaae55bbfcf1f9d005ae05b7490b5b802
1f10210366 <85672478+1f10210366@users.noreply.github.com>
2023-10-11 15:21:01 +0900

    chatgpt実装

--- team4_chatgpt/views.py
+++ team4_chatgpt/views.py
@@ -3,15 +3,47 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 from .forms import ChatForm
+import openai
+
 
 def index(request):
   
 
-  form = ChatForm()
-  template = loader.get_template('team4_chatgpt/index.html')
-  context = {
-    'form':form
+  # 応答結果
+  chat_result = ""
+
+  if request.method == "POST":
+    #chatgptボタンが押された場合
+
+    form = ChatForm(request.POST)
+    if form.is_valid():
+      sentence = form.cleaned_data['sentence']
+
+      openai.api_key = "wprzlNppD6DwlmSTfw35yGnUyaQ8XwfqnFKAnxB6WAjOaRp641FrM91T_NY1E05F6DyW6iIMsmJVSjuyKU8NZsg"
+      openai.api_base = "https://api.openai.iniad.org/api/v1"
+
+      response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"{sentence}"}
+        ]
+    )
+    
+    chat_results = response["choices"][0]["message"]["content"]
+
+  
+  else:
+    form = ChatForm()
+    
+    template = loader.get_template('team4_chatgpt/index.html')
+    context = {
+    'form':form,
+    'chat_results':chat_results
   }
+
+    
+
   return HttpResponse(template.render(context,request))
 
 
