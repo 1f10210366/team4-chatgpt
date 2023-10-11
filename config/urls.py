a5579c81f084af366cff9552468ade517ede8396
1f10210366 <85672478+1f10210366@users.noreply.github.com>
2023-10-11 14:54:22 +0900

    設定

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+import team4_chatgpt.views
+
+
 
 urlpatterns = [
+    path('',include("team_chatgpt.urls")),
     path('admin/', admin.site.urls),
 ]
+
+
