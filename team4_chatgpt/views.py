4d4ebcc30d6f2768c18f9b4e98c0e9bab35b1fba
1f10210366 <85672478+1f10210366@users.noreply.github.com>
2023-10-11 15:28:27 +0900

    修正

--- team4_chatgpt/views.py
+++ team4_chatgpt/views.py
@@ -1,49 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
 from .forms import ChatForm
 import openai
 
-
 def index(request):
-  
+    chat_results = ""  # 'chat_results' の初期化
 
-  # 応答結果
-  chat_result = ""
+    if request.method == "POST":
+        form = ChatForm(request.POST)
+        if form.is_valid():
+            sentence = form.cleaned_data['sentence']
 
-  if request.method == "POST":
-    #chatgptボタンが押された場合
+            openai.api_key = "wprzlNppD6DwlmSTfw35yGnUyaQ8XwfqnFKAnxB6WAjOaRp641FrM91T_NY1E05F6DyW6iIMsmJVSjuyKU8NZsg"
+            openai.api_base = "https://api.openai.iniad.org/api/v1"
 
-    form = ChatForm(request.POST)
-    if form.is_valid():
-      sentence = form.cleaned_data['sentence']
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": f"{sentence}"}
+                ]
+            )
 
-      openai.api_key = "wprzlNppD6DwlmSTfw35yGnUyaQ8XwfqnFKAnxB6WAjOaRp641FrM91T_NY1E05F6DyW6iIMsmJVSjuyKU8NZsg"
-      openai.api_base = "https://api.openai.iniad.org/api/v1"
+            chat_results = response["choices"][0]["message"]["content"]
 
-      response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"{sentence}"}
-        ]
-    )
-    
-    chat_results = response["choices"][0]["message"]["content"]
+    else:
+        form = ChatForm()
 
-  
-  else:
-    form = ChatForm()
-    
-  template = loader.get_template('team4_chatgpt/index.html')
-  context = {
-    'form':form,
-    'chat_results':chat_results
-  }
+    template = loader.get_template('team4_chatgpt/index.html')
+    context = {
+        'form': form,
+        'chat_results': chat_results  # 変数名を 'chat_results' に修正
+    }
 
-    
-
-  return HttpResponse(template.render(context,request))
-
-
+    return HttpResponse(template.render(context, request))
