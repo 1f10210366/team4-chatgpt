b2b8b24d2edbaa160997f6e2ff15b5a788187aff
1f10210366 <85672478+1f10210366@users.noreply.github.com>
2023-10-25 15:42:22 +0900

    修正

--- team4_chatgpt/urls.py
+++ team4_chatgpt/urls.py
@@ -5,6 +5,6 @@ app_name = "team4_chatgpt"
 urlpatterns = [
   path("",views.TopView.as_view(),name="top"),
   path("index/", views.index, name="index"),
-  path("login/", views.login, name="login"),  # 'login' パターンを追加
-  path("signup/", views.signup, name="signup"),  # 例：'signup' パターンも追加
+  path("login/", views.LoginView.as_view(), name="login"),  # 'login' パターンを追加
+  path("signup/", views.SignUpView.as_view(), name="signup"),  # 例：'signup' パターンも追加
 ]
