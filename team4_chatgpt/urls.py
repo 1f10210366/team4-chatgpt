c19bb688064afe45e3b61534e2ac483923a0c420
1f10210366 <85672478+1f10210366@users.noreply.github.com>
2023-10-25 14:55:07 +0900

    home(html,css)

--- team4_chatgpt/urls.py
+++ team4_chatgpt/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "team4_chatgpt"
 urlpatterns = [
-  path("",views.index,name="index")
+  path("",views.index,name="top"),
+  path("index/", views.HomeView.as_view(), name="index"),
 ]
