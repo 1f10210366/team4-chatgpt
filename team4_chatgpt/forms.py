480696c6522ffb7e576254e43d2982cc4ac6851f
1f10210366 <85672478+1f10210366@users.noreply.github.com>
2023-11-01 15:45:03 +0900

    Update forms.py

--- team4_chatgpt/forms.py
+++ team4_chatgpt/forms.py
@@ -20,3 +20,14 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+# forms.py
+
+from django import forms
+
+class ChatForm(forms.Form):
+    subject = forms.CharField(max_length=100, required=True)
+    difficulty = forms.ChoiceField(choices=[("easy", "Easy"), ("medium", "Medium"), ("hard", "Hard")], required=True)
+    num_questions = forms.IntegerField(min_value=1, max_value=10, required=True)
+    sentence = forms.CharField(widget=forms.Textarea, required=True)
+
